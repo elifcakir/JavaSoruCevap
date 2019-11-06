84f8e797da5271e8da9ecf7d2077c62b832c0d90
elif cakir <cakirelif.ec@gmail.com>
2019-11-06 16:27:23 +0300

    üç sayı ortalaması

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int x,y,z;
+        double ortalama;
+
+        Scanner tara =new Scanner(System.in);
+
+        System.out.println("birinci sayıyı giriniz");
+        x=tara.nextInt();
+
+        System.out.println("ikinci sayıyı giriniz");
+        y=tara.nextInt();
+
+        System.out.println("üçüncü sayıyı giriniz");
+        z=tara.nextInt();
+
+        ortalama=(x+y+z)/3d;
+
+        System.out.println("sonuç ="+ ortalama);
 
     }
 }
